d05ff5180f7fd7ef445ed931db024c6a021cae2f
sonhaaa <nguyensonha.hanz@gmail.com>
2021-11-27 12:27:56 +0700

    Test format code with lint-staged

--- src/generate.js
+++ src/generate.js
@@ -1,8 +1,12 @@
 const backgrounds = ['BG_1', 'BG_2', 'BG_3', 'BG_4', 'BG_5']
 const noses = ['NOSE_1', 'NOSE_2', 'NOSE_3']
-const hairs = ['HAIR_1', 'HAIR_2', 'HAIR_3', 'HAIR_4']
+const hairs = ['HAIR_1', 
 
-const start = parseInt('111')
+
+
+'HAIR_2', 'HAIR_3', 'HAIR_4']
+
+const start = parseInt("111")
 const end = parseInt(`${backgrounds.length}${noses.length}${hairs.length}`)
 
 console.log(start, end)
