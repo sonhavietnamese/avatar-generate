a4e3ebd92f48e8b1931abb29063817eddf56f4d4
sonhaaa <nguyensonha.hanz@gmail.com>
2021-11-27 12:32:53 +0700

    Test husky hooks

--- src/generate.js
+++ src/generate.js
@@ -1,12 +1,11 @@
 const backgrounds = ['BG_1', 'BG_2', 'BG_3', 'BG_4', 'BG_5']
 const noses = ['NOSE_1', 'NOSE_2', 'NOSE_3']
-const hairs = ['HAIR_1', 
+const hairs = ["HAIR_1", "HAIR_2", 'HAIR_3', 'HAIR_4',]
 
 
 
-'HAIR_2', 'HAIR_3', 'HAIR_4']
 
-const start = parseInt("111")
+const start = parseInt('111')
 const end = parseInt(`${backgrounds.length}${noses.length}${hairs.length}`)
 
 console.log(start, end)
--- src/index.js
+++ src/index.js
@@ -2,11 +2,11 @@
     const fs = require('fs')
     const { createCanvas, loadImage } = require('canvas')
 
-    const canvasWidth = 361
+const canvasWidth = 361
     const canvasHeight = 361
     const canvasBackgroundColor = '#FF0000'
 
-    const bodyPosition = { x: 87, y: 270 }
+    const bodyPosition = { x: 87, y: 270      }
     const hairPosition = { x: 87, y: 69 }
     const facePosition = { x: 123, y: 122 }
     const nosePosition = { x: 176, y: 184 }
